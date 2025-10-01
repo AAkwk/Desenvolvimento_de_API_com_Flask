0fcd6640d07211b13161a97db5770a3cc2e7c0c8
AAkwk <aakawakitas12@gmail.com>
2025-10-01 09:57:43 -0400

    autenticacao

--- dio_bank/src/app.py
+++ dio_bank/src/app.py
@@ -1,23 +1,41 @@
+import os
+from datetime import datetime
+
 import click
 import sqlalchemy as sa
 from flask import Flask, current_app
 from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy.orm import DeclarativeBase , Mapped, mapped_column
+from sqlalchemy.orm import DeclarativeBase , Mapped, mapped_column, relationship
 from flask_migrate import Migrate
-
+from flask_jwt_extended import JWTManager
 
 class Base(DeclarativeBase):
   pass
 
 db = SQLAlchemy(model_class=Base)
 migrate = Migrate()
+jwt = JWTManager()
+
+class Role(db.Model):
+
+    id: Mapped[int] = mapped_column(sa.Integer, primary_key=True)
+    name: Mapped[str] = mapped_column(sa.String, nullable=False)
+    user: Mapped[list["User"]] = relationship(back_populates="role")
+
+    def __repr__(self) -> str:
+        return f"Role(id={self.id!r}, name={self.name!r})"
 
 class User(db.Model):
     id: Mapped[int] = mapped_column(sa.Integer, primary_key=True)
     username: Mapped[str] = mapped_column(sa.String, unique=True, nullable=False)
-    active: Mapped[bool] = mapped_column(sa.Boolean, default=True)
+    password: Mapped[str] = mapped_column(sa.String, nullable=False)
+    role_id: Mapped[int] = mapped_column(sa.ForeignKey('role.id'))
+    role: Mapped["Role"] = relationship(back_populates="user")
+
+
     def __repr__(self) -> str: 
-        return f"User(id={self.id!r}, username={self.username!r}, active={self.active!r})"
+        return f"User(id={self.id!r}, username={self.username!r})"
+
 
 
 
@@ -44,6 +62,7 @@ def create_app(test_config=None):
     app.config.from_mapping(
         SECRET_KEY='dev',
         SQLALCHEMY_DATABASE_URI='sqlite:///blog.sqlite',
+        JWT_SECRET_KEY='JWT_SECRET_KEY'  # Change this IRL
     )
 
     if test_config is None:
@@ -59,10 +78,15 @@ def create_app(test_config=None):
     # initialize extensions
     db.init_app(app)
     migrate.init_app(app, db)
+    jwt.init_app(app)
+
 
     #register blueprints
-    from src.controllers import user
-    app.register_blueprint(user.app)
+    from src.controllers import user , auth , role
     
+
+    app.register_blueprint(user.app)
+    app.register_blueprint(auth.app)
+    app.register_blueprint(role.app)
 
     return app
--- dio_bank/src/controllers/user.py
+++ dio_bank/src/controllers/user.py
@@ -2,33 +2,45 @@ from flask import Blueprint, request
 from src.app import User, db
 from http import HTTPStatus
 from sqlalchemy import inspect
-
+from flask_jwt_extended import jwt_required
+from src.utils import requires_role
 
 app = Blueprint('user', __name__, url_prefix='/users')
 
-def create_user():
+def _create_user():
     data = request.json
-    user = User(username=data["username"])
+    user = User(
+        username=data["username"],
+        password=data["password"],  
+        role_id=data["role_id"],
+         )
     db.session.add(user)
     db.session.commit()
 
 def _list_users():
-    querry = db.select(User)
-    users = db.session.execute(querry).scalars()
-    return [{
-        "id": user.id,
-        "username": user.username
-    } for user in users  
+    query = db.select(User)
+    users = db.session.execute(query).scalars()
+    return [
+        {
+            "id": user.id,
+            "username": user.username,
+            "role":{
+                "id": user.role.id, 
+                "name": user.role.name,} 
+        } 
+    for user in users  
     ]
      
 
 @app.route('/', methods=['GET', 'POST'])
-def handle_users():
+@jwt_required()
+@requires_role("admin")
+def list_or_create_user():
     if request.method == 'POST':
-        create_user()
+        _create_user()
         return{"message": "User created successfully"}, HTTPStatus.CREATED
     else:
-        return{"users":_list_users()}
+        return{ "users": _list_users()}
 
 @app.route('/<int:user_id>')
 def get_user(user_id):
